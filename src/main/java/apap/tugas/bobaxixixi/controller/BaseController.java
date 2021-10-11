d796901e0af2a82598c19469a826413d5fdbe4d1
Rizky Athallah <muhammad.rizky@ui.ac.id>
2021-10-11 21:31:29 +0700

    Initial commit

---
+++ src/main/java/apap/tugas/bobaxixixi/controller/BaseController.java
@@ -0,0 +1,14 @@
+package apap.tugas.bobaxixixi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+
+    @GetMapping("/")
+        public String home(){
+        return "home";
+    }
+
+}
---
+++ src/main/java/apap/tugas/bobaxixixi/controller/BobaController.java
@@ -0,0 +1,7 @@
+package apap.tugas.bobaxixixi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class BobaController {
+
+}
